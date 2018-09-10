7d712b66ec2fed9cdf75bde6af4d3bcdf6c076a8
zane <zhenhao886@163.com>
2018-09-10 17:54:49 +0800

    111

--- app/src/main/java/com/example/hao/accessibilityservice/MainActivity.java
+++ app/src/main/java/com/example/hao/accessibilityservice/MainActivity.java
@@ -8,7 +8,7 @@ import android.text.TextUtils;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-    public String TAG="ZaneSercices";
+    public String TAG="ZaneSercices111111";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
