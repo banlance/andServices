4e2284a05bd4ec1758275c0f5189b0f2c7e189c4
zane <zhenhao886@163.com>
2018-09-12 15:01:06 +0800

    111

--- app/src/main/java/com/example/hao/accessibilityservice/MyServices.java
+++ app/src/main/java/com/example/hao/accessibilityservice/MyServices.java
@@ -2,50 +2,66 @@ package com.example.hao.accessibilityservice;
 
 import android.accessibilityservice.AccessibilityService;
 import android.accessibilityservice.AccessibilityServiceInfo;
+import android.content.IntentFilter;
+import android.drm.DrmStore;
 import android.os.Build;
+import android.os.Handler;
 import android.support.annotation.RequiresApi;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
+import android.webkit.WebView;
 
 import java.util.List;
 
 public class MyServices extends AccessibilityService {
     public String TAG = "ZaneSercices";
-    private boolean isCliking=true;
-    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+    private boolean isCliking = true;
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
-//        String pagName = event.getPackageName().toString();
-//        int eventType = event.getEventType();
-//        Log.d(TAG, "onAccessibilityEvent: .");
-//        Log.d(TAG, "pagName: =" + pagName);
-//        Log.d(TAG, "eventType: " + eventType);
-//        switch (eventType) {
-//            case AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED:
-//                break;
-//        }
-        //event.setEventTime(3500);
+
         AccessibilityNodeInfo rootNode = getRootInActiveWindow();
         if (rootNode != null) {
             List<AccessibilityNodeInfo> nodes = rootNode.findAccessibilityNodeInfosByText("评");
+            List<AccessibilityNodeInfo> listNodesInfos = rootNode.findAccessibilityNodeInfosByViewId("com.jifen.qukan:id/a9u");
+            if (listNodesInfos.size() > 0) {
+                int childCount = listNodesInfos.get(0).getChildCount();
+                if (childCount > 0) {
+
+
+                    if (listNodesInfos.get(0).getChild(0).getClassName().equals("android.webkit.WebView")) {
+                        if (listNodesInfos.get(0).getChild(0).getChildCount() > 0) {
+                            if (null != listNodesInfos.get(0).getChild(0).getChild(0)) {
+                                if (listNodesInfos.get(0).getChild(0).getChild(0).getClassName().equals("android.webkit.WebView")) {
+                                    listNodesInfos.get(0).getChild(0).getChild(0).performAction(AccessibilityNodeInfo.ACTION_SCROLL_FORWARD);
+                                    new Handler().postDelayed(() -> {
+                                                if (null != listNodesInfos.get(0).getChild(0).getChild(0)) {
+                                                    listNodesInfos.get(0).getChild(0).getChild(0).performAction(AccessibilityNodeInfo.ACTION_SCROLL_BACKWARD);
+                                                }
+                                            },
+                                            3000);
+
+
+                                }
+                            }
+                        }
+                    }
+                }
+            }
             int count = nodes.size();
             AccessibilityNodeInfo node;
-            if (count > 0&&isCliking) {
-                isCliking=false;
+            if (count > 0 && isCliking) {
+                isCliking = false;
                 for (int i = 0; i < count; i++) {
                     node = nodes.get(i);
                     if (node.getText().toString().contains("评")) {
-                        try {
-                            Thread.sleep(3000);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
                         //performGlobalAction(AccessibilityNodeInfo.ACTION_SCROLL_FORWARD);
-                         node.getParent().performAction(AccessibilityNodeInfo.ACTION_CLICK);
+                        node.getParent().performAction(AccessibilityNodeInfo.ACTION_CLICK);
 
-                        isCliking=true;
+                        isCliking = true;
                         break;
                     }
                 }
